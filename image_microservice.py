5ca8db531f6afd385d6a978c21e6d5640048b17b
Michelle K <81453962+mvkwong@users.noreply.github.com>
2022-05-12 17:59:29 -0700

    change microservice trigger from function call to text file instruction

--- image_microservice.py
+++ image_microservice.py
@@ -1,13 +1,29 @@
 import random
 
-def get_image():
-    file = open("manga_covers.txt", "r")
-    URL_list = []
-    for line in file.readlines():
-        URL_list.append(line.strip())
-    file.close()
-    rand_val = random.randint(0, len(URL_list)-1)
-    manga_URL = URL_list[rand_val]
+while True:
+    file = open("instructions.txt", "r")
+    if file.readline().strip() == "run":
 
-    file = open("URL.txt", "w")
-    file.write(manga_URL)
+        # Delete contents in instructions.txt so that new URLs are not generated nonstop
+        file = open("instructions.txt", "w")
+        file.write("")
+        file.close()  
+
+        # Read txt file that contains cover URLs and convert to a list of URLs
+        file = open("./manga_covers.txt", "r")
+        URL_list = []
+        for line in file.readlines():
+            URL_list.append(line.strip())
+        file.close()
+
+        # Generated a pseudo-random number and use as index number to retrieve URL at that index
+        rand_val = random.randint(0, len(URL_list)-1)
+        manga_URL = URL_list[rand_val]
+
+        # Write URL to txt file
+        file = open("URL.txt", "w")
+        file.write(manga_URL)
+        file.close()
+        
+    else:
+        file.close()
